874a67a3f13b6ef40b852cb66fd8fd28bb069b2b
HarshaV-Spektra <Harsha.Vardhan@spektrasystems.com>
2026-07-09 23:48:57 +0530

    Refactor script by removing unused code and functions
    Removed unused UPN parsing and credential creation functions from the script. Streamlined the script by eliminating unnecessary comments and function calls.

--- SQLwarehouse/psscript01.ps1
+++ SQLwarehouse/psscript01.ps1
@@ -15,10 +15,6 @@ Param (
     [string]$AppSecret
 )
 
-# Basic parsing of UPN (kept in case you need tenant name later)
-$Inputstring = $AzureUserName
-$CharArray   = $InputString.Split("@")
-
 Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
 [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 Write-Output "TLS setting: $([Net.ServicePointManager]::SecurityProtocol)"
@@ -30,54 +26,16 @@ Write-Output "TLS setting: $([Net.ServicePointManager]::SecurityProtocol)"
 [System.Environment]::SetEnvironmentVariable('AzureSubscriptionID', $AzureSubscriptionID, [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('AzureTenantID', $AzureTenantID, [System.EnvironmentVariableTarget]::Machine)
 
-# CloudLabs common functions
-
 $path = (Get-Location).Path
 $commonscriptpath = "$path\cloudlabs-common\cloudlabs-windows-functions.ps1"
 . $commonscriptpath
 
-#InstallManualStatusAgent
-CloudlabsManualAgent Install
-
-# Run Imported functions from cloudlabs-windows-functions.ps1
-WindowsServerCommon
-
-Function CreateCredFile($AzureUserName, $AzurePassword, $AzureTenantID, $AzureSubscriptionID, $DeploymentID, $AppID, $AppSecret)
-{
-    New-Item -ItemType directory -Path C:\LabFiles -force
-
-    $WebClient = New-Object System.Net.WebClient
-    $WebClient.DownloadFile("https://raw.githubusercontent.com/harshavardhan-spektra/MSDN-template/refs/heads/main/SQLwarehouse/AzureCreds.txt","C:\LabFiles\AzureCreds.txt")
-    $WebClient.DownloadFile("https://raw.githubusercontent.com/harshavardhan-spektra/MSDN-template/refs/heads/main/SQLwarehouse/AzureCreds.ps1","C:\LabFiles\AzureCreds.ps1")
-
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzureUserNameValue", "$AzureUserName"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzurePasswordValue", "$AzurePassword"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzureTenantIDValue", "$AzureTenantID"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AzureSubscriptionIDValue", "$AzureSubscriptionID"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "DeploymentIDValue", "$DeploymentID"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AppIDValue", "$AppID"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AppSecretValue", "$AppSecret"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-
-    (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzureUserNameValue", "$AzureUserName"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzurePasswordValue", "$AzurePassword"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzureTenantIDValue", "$AzureTenantID"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "AzureSubscriptionIDValue", "$AzureSubscriptionID"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.ps1") | ForEach-Object {$_ -Replace "DeploymentIDValue", "$DeploymentID"} | Set-Content -Path "C:\LabFiles\AzureCreds.ps1"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AppIDValue", "$AppID"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-    (Get-Content -Path "C:\LabFiles\AzureCreds.txt") | ForEach-Object {$_ -Replace "AppSecretValue", "$AppSecret"} | Set-Content -Path "C:\LabFiles\AzureCreds.txt"
-
-    Copy-Item "C:\LabFiles\AzureCreds.txt" -Destination "C:\Users\Public\Desktop"
-}
-
-CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $AppID $AppSecret
-InstallModernVmValidator
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID
 
 
 # Enable CloudLabs Embedded Shadow Feature (trainer ↔ VM)
 
 Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
-
-InstallChocolatey
 
 #Enable Autologon
 $AutoLogonRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
